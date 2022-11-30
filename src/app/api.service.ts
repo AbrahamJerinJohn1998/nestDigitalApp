55c2a898ca73aa16ebc60413f08a1a346406be82
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-30 16:08:35 +0530

    service and api added

---
+++ src/app/api.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http:HttpClient) { }
+  fetchCourses=()=>
+  {
+    return this.http.get("https://mylinkurcodesapp.herokuapp.com/getcourses")
+  }
+  fetchFriends=()=>
+  {
+    return this.http.get("https://dummyapifriends.herokuapp.com/view")
+  }
+  addFriends=(dataToSend:any)=>
+  {
+    return this.http.post("https://dummyapifriends.herokuapp.com/adddata",dataToSend)
+  }
+  addCourses=(dataToSend:any)=>
+  {
+    return this.http.post("https://mylinkurcodesapp.herokuapp.com/addcourse",dataToSend)
+  }
+}
--- src/app/employee-login/employee-login.component.ts
+++ src/app/employee-login/employee-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-login',
@@ -9,13 +10,14 @@ export class EmployeeLoginComponent {
 
   userName=""
   password=""
+  constructor(private route:Router){}
   readValues=()=>
   {
     let data:any={"userName":this.userName,"password":this.password}
     console.log(data);
     if (this.userName=="1122"&&this.password=="12345") {
       alert("valid login")
-      
+      this.route.navigate(['/addfriends'])
     } else {
       alert("Invalid credentials")
     }
