21b37d5dc4a686d8887eb10e6e778999a4a59fb4
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-30 12:48:13 +0530

    admin login

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent {
+
+  userName=""
+  password=""
+  readValues=()=>
+  {
+    let data:any={"userName":this.userName,"password":this.password}
+    console.log(data);
+    if (this.userName=="admin"&&this.password=="Nestdigital") {
+      alert("valid login")
+      
+    } else {
+      alert("Invalid credentials")
+    }
+
+    
+  }
+  
+}
